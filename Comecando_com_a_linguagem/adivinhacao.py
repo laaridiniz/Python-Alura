481dc8a9b59452a8faa12338ad3068111573177f
Larissa Diniz <diniz.laari@gmail.com>
2023-08-13 10:18:24 -0300

    Add files via upload

---
+++ Comecando_com_a_linguagem/adivinhacao.py
@@ -0,0 +1,34 @@
+print("**************************************")
+print("Seja bem-vindo ao jogo de adivinhação!")
+print("**************************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+
+##while(rodada <= total_de_tentativas):
+##    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+##    chute_str = input("Digite um número de 0 a 100: ")
+##    print("Você digitou ", chute_str)
+##    chute = int(chute_str)
+
+for rodada in range(1,total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número de 0 a 100: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Parabéns! Você acertou o número secreto!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        else:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+
+print("Fim do jogo.")
